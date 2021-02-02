cf8b4f1b658ff797f2d45aac3a59d88cafd74db7
Guus Kolpa <guus.kolpa@gmail.com>
2021-02-02 10:22:23 +0100

    made name requirement more explicit

--- bot_resources/standard_messages.py
+++ bot_resources/standard_messages.py
@@ -1,7 +1,8 @@
 netplay_tournament_start = """
+!!!YOUR NAME HAS TO BE IDENTICAL ON BOTH CHALLONGE AND DISCORD OR YOU WILL BE DQ'D!!!
 @here {}
 Netplay tournament #{} tonight at 20:00, you know the deal, Bo5 to be announced based on entrant number.
 For questions contact <@186260943682928641> or <@118886597293768709>
 
-ps: Remember your name has to be identical on both challonge and discord or you WILL BE DQ'D.
+!!!YOUR NAME HAS TO BE IDENTICAL ON BOTH CHALLONGE AND DISCORD OR YOU WILL BE DQ'D!!!
 """
