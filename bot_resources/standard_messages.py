9b2c5d9f16ad7c4f098d9b700901fdf03cff2f14
Guus Kolpa <guus.kolpa@gmail.com>
2021-02-16 12:00:37 +0100

    fixed message

--- bot_resources/standard_messages.py
+++ bot_resources/standard_messages.py
@@ -2,7 +2,7 @@ netplay_tournament_start = """
 !!!YOUR NAME HAS TO BE IDENTICAL ON BOTH CHALLONGE AND DISCORD OR YOU WILL BE DQ'D!!!
 @here {}
 Netplay tournament #{} tonight at 20:00, you know the deal, Bo5 to be announced based on entrant number.
-For questions contact <@186260943682928641> or <@118886597293768709>
+For questions contact <@118886597293768709>
 
 !!!YOUR NAME HAS TO BE IDENTICAL ON BOTH CHALLONGE AND DISCORD OR YOU WILL BE DQ'D!!!
 """
